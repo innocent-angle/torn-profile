67cdb63fd373819d5a269021961bd9a4a9d6cec1
me <spaceprincess213@gmail.com>
2024-03-14 06:29:48 -0400

    bye bye nuke :(

--- company_spy.js
+++ company_spy.js
@@ -1,16 +1,14 @@
 clear();
 
 {
-  let income = 73872820;
-  
-  for (let i = 150; i < 250; i++) {
-      if ((income / i) % 1 == 0) {
-        const amount = income / i;
-        const price = i;
+	let income = 73872820;
 
-        console.log(
-          `${amount} sold for $${price}`
-      );
-    }
-  } 
+	for (let i = 150; i < 250; i++) {
+		if ((income / i) % 1 == 0) {
+			const amount = income / i;
+			const price = i;
+
+			console.log(`${amount} sold for $${price}`);
+		}
+	}
 }
